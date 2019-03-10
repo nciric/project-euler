3c6167bb25e462d13eee64846cc029535fd3db56
Nebojša Ćirić <nciric@gmail.com>
2019-03-10 13:15:40 -0700

    initial rust commit

---
+++ rust/p1/src/main.rs
@@ -0,0 +1,24 @@
+/*
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+ * get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * cargo run
+ */
+
+fn find_solution() -> i32 {
+  let mut sum = 0i32;
+
+  for (_,j) in (1..1000).enumerate() {
+      if j % 3 == 0 || j % 5 == 0 {
+          sum += j;
+      }
+  }
+
+  return sum;
+}
+
+fn main() {
+  println!("Solution: {}", find_solution());
+}
