ab12fbd9178d2e5513ce04a6c0b699022407bd07
Nebojša Ćirić <nciric@gmail.com>
2021-08-14 17:54:38 -0700

    Initial checkin.

---
+++ rust/p107/src/main.rs
@@ -0,0 +1,11 @@
+/*
+ * Project Euler problem 107.
+ * It has associated file p107_network.txt with graph info.
+ * Problem boils down to minimum spanning tree, and solution should be O(mLogn) where m are edges and n are vertices.
+ * 
+ * cargo run
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
